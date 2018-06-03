d727f67ffd8f20dd24a6e8787a7d186794b3dd95
Wiktoria Jabłońska <30338072+JabWik@users.noreply.github.com>
2018-06-03 15:54:47 +0200

    Reworked tabMap functions
    - moved tabMap from main.c to tabMap.c;
    - recoded tabGenerator;
    - completed all basic functions for generating a map;

--- main.c
+++ main.c
@@ -2,72 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include "stats.h"
+#include "tabMap.h"
 
-
-#define HEIGHT 11
-#define WIDTH 11
-
-enum
-{
-    tabEnd,
-    attack,
-    empty,
-    treasure,
-    challenge,
-    shop,
-    visited,
-    last,
-    wolf,
-    sheep,
-    rabbit,
-};
-
-
-void tabGenerator(int t[HEIGHT][WIDTH])
-{
-    for (int i = 0; i < HEIGHT; i++) //wyzerowywanie krańców mapy
-    {
-        t[i][0] = 0;
-        t[i][WIDTH-1] = 0;
-    }
-
-    for (int i = 0; i < WIDTH; i++)
-    {
-        t[0][i] = 0;
-        t[HEIGHT-1][i] = 0;
-    }
-
-    for (int i = 1; i < HEIGHT-1; i++) //generowanie ogólne mapy
-    {
-        for (int j = 1; j < WIDTH-1; j++)
-        {
-            t[i][j] = rand()%5 + 1;
-        }
-    }
-
-    for (int i = 1; i < HEIGHT-1; i++) //szczegolny przypadek dla attack
-    {
-        for (int j = 1; j < WIDTH-1; j++)
-        {
-            if(t[i][j] == attack)
-            {
-                t[i][j] = rand()%3 + last;
-            }
-        }
-    }
-}
-
-void tabShow(int t[HEIGHT][WIDTH]) //pokaz mape
-{
-    for (int i = 0; i < HEIGHT; i++)
-    {
-        for (int j = 0; j < WIDTH; j++)
-        {
-            printf("%d ",t[i][j]);
-        }
-        printf("\n");
-    }
-}
+struct stats player;
 
 int action()
 {
@@ -75,63 +12,120 @@ int action()
     static int foolish = 0;
 
     printf("\n\n\nW którą stronę chcesz iść?");
-    choice:
+choice:
     if (foolish > 3)
     {
         printf("Zginales przez wlasna glupote!!!");
         getchar();
         exit(0);
     }
-    printf("\n1. W lewo.\n2. W prawo.\n3.W górę.\n4. W dół.\n5. Popełnij samobójstwo.");
+    printf("\n1. W lewo.\n2. W prawo.\n3.W górę.\n4. W dół.\n5. Popełnij samobójstwo.\n");
     scanf("%d", &x);
     switch(x)
     {
-        case 1:
-        {
-            return -1;
-        }
-        case 2:
-        {
-            return +1;
-        }
-        case 3:
-        {
-            return -WIDTH;
-        }
-        case 4:
-        {
-            return +WIDTH;
-        }
-        case 5:
-        {
-            exit(0);
-        }
-        default:
-        {
+    case 1:
+    {
+        return -1;
+    }
+    case 2:
+    {
+        return +1;
+    }
+    case 3:
+    {
+        return -WIDTH;
+    }
+    case 4:
+    {
+        return +WIDTH;
+    }
+    case 5:
+    {
+        exit(0);
+    }
+    default:
+    {
         foolish++;
         printf("Nie wyglupiaj sie!!!");
         goto choice;
-        }
+    }
     }
 }
 
+int fight(int who)
+{
+    struct stats enemy;
+    enemy.attackpoint = 1;
+    enemy.healt = 10;
+    while(enemy.healt > 0 && player.healt > 0)
+    {
+        printf("statsy z walki");
+        enemy.healt += -player.attackpoint;
+        player.healt += -enemy.attackpoint;
+    }
+}
+
+void event(int number)
+{
+    switch(number)
+    {
+    case tabEnd:
+    {
+        printf("wygrales\n");
+        exit(0);
+    }
+    case attack:
+    {
+        printf("wpadles na królika\n");
+        fight(number);
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
+}
 
 
 int main()
 {
     srand(time(NULL));
 
-    int tab[HEIGHT][WIDTH];
+    int choice;
+    printf("1.nowa gra\n2.naziwsko\nanykey.quit\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+    case 1:
+    {
+        printf("1.pierwszapostac\n2.drugapostac\n");
+        scanf("%d",&choice);
+        player.healt = 100;
+        player.attackpoint = 2;
+        break;
+    }
+    case 2:
+    {
+        printf("wiktoria jabłońska\n");
+        return 0;
+    }
+    default:
+    {
+        return 0;
+    }
+    }
 
+    int tab[HEIGHT][WIDTH];
     tabGenerator(tab);
-    tab[5][5] = visited;
 
     int *ptr = &tab[5][5];
-
     tabShow(tab);
-
-    ptr += action();
-    printf("%d\n", *ptr);
-
+    while(1)
+    {
+        ptr += action();
+        event( *ptr );
+        //printf("%d\n", *ptr);
+    }
     return 0;
 }
