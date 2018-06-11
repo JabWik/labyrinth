dae2a4a727d2666c5591c9e1dc9fd1f03eedffaf
Wiktoria Jabłońska <30338072+JabWik@users.noreply.github.com>
2018-06-11 12:06:09 +0200

    Created global.h
    - defined global variables;
    - wrote down a full list of used libraries;

---
+++ global.h
@@ -0,0 +1,18 @@
+#ifndef GLOBAL_H_INCLUDED
+#define GLOBAL_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "stats.h"
+#include <stdbool.h>
+
+#define HEIGHT 11
+#define WIDTH 11
+
+stats player;
+
+bool test;
+
+#endif // GLOBAL_H_INCLUDED
