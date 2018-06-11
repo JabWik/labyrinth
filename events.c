47f6b21865a4219ff7b6547cd3e95cd39eedadb5
Wiktoria Jabłońska <30338072+JabWik@users.noreply.github.com>
2018-06-11 14:45:24 +0200

    Completed & updated events.c
    - completed major functions for events;
    - now every event has its own effect;
    - the "end of game password" is working;
    - added different death summary in the output file;

--- events.c
+++ events.c
@@ -1,19 +1,26 @@
 #include "events.h"
 #include <stdio.h>
 
-void fight(stats enemy)
+
+void fight(stats enemy) //fight
 {
     while(enemy.health > 0 && player.health > 0)
     {
-        printf("statsy z walki:");
+        printf("\nTwoje zdrowie: %i", player.health);
+        printf("\nZdrowie przeciwnika: %i", enemy.health);
         enemy.health += -player.attackpoints;
         player.health += -enemy.attackpoints;
     }
 
     if(player.health <= 0)
     {
-        printf("you are dead\n");
-        exit(0);
+    printf("ZOSTAŁEŚ ZABITY\n");
+
+    FILE *f = fopen("summary", "w");
+    fprintf(f, "\nZginąłeś, pokonany przez straszliwą bestię... straciłeś wszystko co miałeś.");
+    fclose(f);
+
+    exit(0);
     }
 }
 
@@ -78,27 +85,29 @@ void event(int *p, bool *quit) //map events
     {
     case tabEnd:
     {
-        char password[15] = "TETRIANDOCH";
-        char userword[15];
-        int ch=0;
-
-        int i = 0;
+        char password[30] = "TETRIANDOCH";
+        char userword[30];
 
         printf("\nPodaj hasło, śmiertelniku!");
-        while(ch != '\n')
-        {
-            printf("test");
-            ch = getchar();
-            userword[i] = ch;
-            if(i==0)
-                ch=0;
-            ++i;
-        }
-        userword[i] = '\0';
-        printf("Twoje hasło: %s", userword);
 
-        //scanf("%c", ch);
-        *quit = true;
+        scanf("%s", userword);
+
+        int result;
+        result = strcmp(userword, password);
+        if (result == 0)
+        {
+            printf("WYGRAŁEŚ");
+            *quit = true;
+        }
+        else
+        {
+            printf("PRZEGRAŁEŚ");
+            FILE *f = fopen("summary", "w");
+            fprintf(f, "\nZginąłeś, a nadzieja na wolność umarła razem z Tobą...");
+            fclose(f);
+
+            exit(0);
+        }
 
         break;
     }
@@ -112,66 +121,210 @@ void event(int *p, bool *quit) //map events
     }
     case start:
     {
-        printf("YOLO");
+        printf("Początek");
         break;
     }
     case ghost:
     {
-        printf("wpadles na królika\n");
-        //fight();
+
+        printf("DUCH");
+
+        stats ghost;
+        ghost.attackpoints = 5;
+        ghost.health = 30;
+
+        fight(ghost);
+
+        player.victorypoint += 5;
+
         break;
     }
     case mummy:
     {
+        printf("MUMIA");
+
+
         stats mummy;
         mummy.attackpoints = 10;
         mummy.health = 100;
 
         fight(mummy);
+
+        player.victorypoint += 10;
+
         break;
     }
     case minotaur:
     {
+        printf("MINOTAUR");
+
+        stats minotaur;
+        minotaur.attackpoints = 20;
+        minotaur.health = 200;
+
+        fight(minotaur);
+
+        player.victorypoint =+ 20;
+
         break;
     }
     case skeleton:
     {
+        printf("SKELETON");
+
+        stats skeleton;
+        skeleton.attackpoints = 15;
+        skeleton.health = 150;
+
+        fight(skeleton);
+
+        player.victorypoint =+ 15;
+
         break;
     }
     case priest:
     {
+        printf("KAPŁAN");
+
+        stats priest;
+        priest.attackpoints = 30;
+        priest.health = 60;
+
+        fight(priest);
+        player.victorypoint = 30;
+
         break;
     }
     case rest:
     {
+        printf("Znajdujesz komnatę.");
+
+        player.might += 3;
+        player.attackpoints += 5;
+
         break;
     }
     case cry:
     {
+        printf("Wpadasz w rozpacz...");
+
+        player.will -= 3;
+        player.attackpoints -= 5;
+
         break;
     }
     case gold:
     {
+        printf("Znalazłeś kupkę złota!");
+
+        player.goldpoint += lottery(1,250);
+
         break;
     }
     case potion:
     {
+        printf("Znalazłeś tajemniczą miksturę!");
+
+        player.health += lottery(1,100);
+
         break;
     }
     case life:
     {
+        int y = lottery(1,50);
+        printf("Twoje życie: %i", player.health);
+        printf("Punkty próby życia: %i", y);
+        if (player.health < y)
+        {
+            printf("ZGINĄŁEŚ");
+            exit(0);
+        }
+        else
+        {
+            printf("PRZEŻYŁEŚ");
+            printf("Dostałeś złoto oraz wróciły Ci siły...");
+
+            player.goldpoint += lottery(50,100);
+            player.victorypoint +=10;
+
+            player.health += 30;
+        }
+
         break;
     }
     case will:
     {
+        int y = lottery(6,14);
+       printf("Twój duch: %i", player.will);
+       printf("Punkty próby ducha: %i", y);
+        if (player.will < y)
+        {
+            printf("ZGINĄŁEŚ");
+            exit(0);
+        }
+        else
+        {
+            printf("PRZEŻYŁEŚ");
+            printf("Dostałeś złoto oraz czujesz, że Ci się uda...");
+
+            player.goldpoint += lottery(50,200);
+            player.victorypoint +=10;
+
+            player.attackpoints += 3;
+        }
+
         break;
     }
     case might:
     {
+        int y = lottery(6,14);
+  printf("Twoje życie: %i", player.health);
+     printf("Punkty próby życia: %i", y);
+        if (player.health < y)
+        {
+            printf("ZGINĄŁEŚ");
+            exit(0);
+        }
+        else
+        {
+            printf("PRZEŻYŁEŚ");
+            printf("Dostałeś złoto oraz wróciły Ci siły...");
+
+            player.goldpoint += lottery(50,100);
+            player.victorypoint +=10;
+
+            player.health += 30;
+        }
         break;
     }
     case figure:
     {
+        printf("Znajdujesz kapliczkę");
+
+        int x;
+        scanf("%d", &x);
+        switch(x)
+        {
+        case 1:
+            {
+                printf("Jesteś pobłogosławiony!");
+
+                player.health += 50;
+                player.attackpoints += 10;
+                player.might += 10;
+                player.will += 10;
+
+                player.victorypoint += 25;
+
+                break;
+            }
+
+        default:
+            {
+                break;
+            }
+        }
+
         break;
     }
     case secret:
