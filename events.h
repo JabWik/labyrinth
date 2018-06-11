faf3a3cbf8283de1bf567502db91d83b685e4835
Wiktoria Jabłońska <30338072+JabWik@users.noreply.github.com>
2018-06-11 12:11:58 +0200

    Created & updated events.h
    - created events.h;
    - updated;

---
+++ events.h
@@ -0,0 +1,13 @@
+#ifndef EVENTS_H_INCLUDED
+#define EVENTS_H_INCLUDED
+
+#include "stats.h"
+#include "tabMap.h"
+#include <stdbool.h>
+
+int action();
+void event(int *p, bool *quit);
+
+
+
+#endif // EVENTS_H_INCLUDED
